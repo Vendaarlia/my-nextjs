21b6eded9e4e7237f851b12ab9869e7b55d944f3
Vendaarlia <94086273+Vendaarlia@users.noreply.github.com>
2024-02-20 17:46:52 +0700

    framer motion implement

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b735d938e60c7118"; }
+/******/ 		__webpack_require__.h = function() { return "cd9a8878dddc775f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,17 @@
 import Image from 'next/image';
+import { motion } from 'framer-motion';
 
 
 function Hero() {
   return (
-    <div className="hero-container">
+    <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 3 }} className="hero-container" >
+        <motion.div initial={{ opacity: 0 }} transition={{ delay: 1 }} animate={{ opacity: 1 }} >
         <Image src='/images/profile.jpg' className='profile-img' width={300} height={300} alt='vendaarlia profile' />
+        </motion.div>
         <div className="hero-text">
-            <p>
+            <motion.p initial={{ opacity: 0 }} transition={{ delay: 2 }} animate={{ opacity: 1 }}>
             While early Sólstafir lyrics delved into Nordic mythology and critiques of organized religion, more recent songs explore their spiritual connection with nature, and lately, mental disorders ranging from depression to alcoholism and the taboo behind men in particular discussing those things for fear of being perceived weak.
-            </p>
+            </motion.p>
             <div className="social-icons">
                 <a href="https://twitter.com/solstafir" aria-label='Twitter' target='_blank' rel='noopener noreferrer'>
                     <i className="fa-brands fa-twitter"></i>
@@ -21,7 +24,7 @@ function Hero() {
                 </a>
             </div>
         </div>
-    </div>
+    </motion.div>
   )
 }
 
