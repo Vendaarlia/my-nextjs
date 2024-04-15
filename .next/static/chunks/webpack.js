d3cabd7f51099e42ecfb062ed865e11b993c1de4
Vendaarlia <94086273+Vendaarlia@users.noreply.github.com>
2024-04-15 16:15:32 +0700

    upadte disco grafi media querry

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "024f5645c95d8f21"; }
+/******/ 		__webpack_require__.h = function() { return "9bc0a9827a1a5dca"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
